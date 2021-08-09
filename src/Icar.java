ba9ab6d2fdcb1362308dc17d296e54ec78efed8f
Taide220643 <59428815+Taide220643@users.noreply.github.com>
2021-08-09 17:00:34 +0700

    Initial commit

---
+++ src/Icar.java
@@ -0,0 +1,6 @@
+public interface Icar {
+	public void engineStart();
+    public void changeGear(int newGear);
+    public void turnLeft();
+    public void turnRight();
+}
---
+++ src/TestDriver.java
@@ -0,0 +1,14 @@
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		car c1 = new car();
+		c1.engineStart();
+		c1.engineStop();
+		
+		Icar c2 = new car();
+		c2.engineStart();
+		
+	}
+
+}
